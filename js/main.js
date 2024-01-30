92f08dfe09f317a89409a5234fbdec0907278812
Jennifer Santana <jennifersouzadesantana@gmail.com>
2024-01-30 15:37:19 -0300

    Adicionei as animaçoes nos seletores(2).

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const options = document.querySelectorAll('.options')
+const content = document.querySelectorAll('.content')
+
+options.forEach((option,index)=>{
+    option.addEventListener('click',()=>{
+        if (!option.classList.contains('expanded')){
+            option.classList.add('expanded')
+            content[index].classList.add('content_show')
+
+            options.forEach((other_option,other_index)=>{
+                if (other_option !== option && other_option.classList.contains('expanded')){
+                    other_option.classList.remove('expanded')
+                    content[other_index].classList.remove('content_show')
+                }
+            })
+        } else {
+            option.classList.remove('expanded')
+            content[index].classList.remove('content_show')
+        }
+    }) 
+})
+
