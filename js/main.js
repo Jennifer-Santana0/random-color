1793865ef4a9e84c13c2cbb374ed84622ed41c73
Jennifer Santana <jennifersouzadesantana@gmail.com>
2024-01-31 10:36:24 -0300

    Adicionei a funcao de adicionar as opçoes do main no layout e deixei os paragrafos do main com 3 linhas(7).

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ const content = document.querySelectorAll('.content')
 const op_header = document.querySelectorAll('.op_header')
 const op_main = document.querySelectorAll('.op_main')
 const op_footer = document.querySelectorAll('.op_footer')
+const content_header = document.querySelector('#content_header')
+const content_main = document.querySelector('#content_main')
+const content_footer = document.querySelector('#content_footer')
 
 options.forEach((option,index)=>{
     option.addEventListener('click',()=>{
@@ -29,17 +32,42 @@ op_header.forEach((option)=>{
         op_header.forEach((other_option)=>{
             other_option.classList.remove('border_green_header')
         })
+        
+        // adicionando no layout_header
+        content_header.innerHTML = ''
+        const clone = option.cloneNode(true)
+        content_header.appendChild(clone)
+
         option.classList.add('border_green_header')
     })
 })
 
 // adicionando a borda verde nos elementos do main
-op_main.forEach((option)=>{
+let validacao_main_1 = true
+let validacao_main_2 = true
+let validacao_main_3 = true
+op_main.forEach((option,index)=>{
     option.addEventListener('click',()=>{
         
-        op_main.forEach((other_option)=>{
-            other_option.classList.remove('border_green_main')
-        })
+        
+        // adicionando no layout_main
+        if (index===0 && validacao_main_1===true){
+            const clone = option.cloneNode(true)
+            clone.classList.add('main_height')
+            content_main.appendChild(clone)
+            validacao_main_1 = false
+        } else if (index===1 && validacao_main_2 === true){
+            const clone = option.cloneNode(true)
+            clone.classList.add('main_height')
+            content_main.appendChild(clone)
+            validacao_main_2 = false
+        }else if (index===2 && validacao_main_3 === true){
+            const clone = option.cloneNode(true)
+            clone.classList.add('main_height')
+            content_main.appendChild(clone)
+            validacao_main_3 = false
+        }
+
         option.classList.add('border_green_main')
     })
 })
