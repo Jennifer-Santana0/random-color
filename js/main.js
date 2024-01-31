07841f8bb618ca5fb64f9f64fa42416e9162ba7c
Jennifer Santana <jennifersouzadesantana@gmail.com>
2024-01-30 21:04:15 -0300

    Criei as opçoes do footer e adicionei a borda verde no elemento selecionado(6).

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const options = document.querySelectorAll('.options')
 const content = document.querySelectorAll('.content')
 const op_header = document.querySelectorAll('.op_header')
+const op_main = document.querySelectorAll('.op_main')
+const op_footer = document.querySelectorAll('.op_footer')
 
 options.forEach((option,index)=>{
     option.addEventListener('click',()=>{
@@ -21,11 +23,34 @@ options.forEach((option,index)=>{
     }) 
 })
 
+// adicionando a borda verde nos elementos do header
 op_header.forEach((option)=>{
     option.addEventListener('click',()=>{
         op_header.forEach((other_option)=>{
-            other_option.classList.remove('border_green')
+            other_option.classList.remove('border_green_header')
         })
-        option.classList.add('border_green')
+        option.classList.add('border_green_header')
+    })
+})
+
+// adicionando a borda verde nos elementos do main
+op_main.forEach((option)=>{
+    option.addEventListener('click',()=>{
+        
+        op_main.forEach((other_option)=>{
+            other_option.classList.remove('border_green_main')
+        })
+        option.classList.add('border_green_main')
+    })
+})
+
+// adicionando a borda verde nos elementos do footer
+op_footer.forEach((option)=>{
+    option.addEventListener('click',()=>{
+        
+        op_footer.forEach((other_option)=>{
+            other_option.classList.remove('border_green_footer')
+        })
+        option.classList.add('border_green_footer')
     })
 })
